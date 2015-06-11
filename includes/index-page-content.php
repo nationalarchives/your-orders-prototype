3324b19e61e6138916f237b3c07427aedb3c4e06
Chris Bishop <domingobishop@gmail.com>
2015-06-11 16:26:49 +0100

    Added container

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -1,12 +1,3 @@
-<?php
-    $titles = array('42ND DIVISION Reconnaissance Battalion', '1ST ARMOURED DIVISION Commander Royal Engineers', '3RD INFANTRY BRIGADE Anti-tank Company', '1ST BATTALION Somerset Light Infantry', 'NAVY Medal card of Seddon, J Corps');
-    $formats = array('Record copy', 'Digital download', 'Naturalisation certificate');
-    $statuses = array('Page check in progress', 'Page check successful', 'Page check cancelled', 'Record copying order in progress', 'Record copying order complete and dispatched');
-    $digStatuses = array('Digital copy ready to download', 'Digital copy download expired');
-    $customerInst = array('Looking for information related to John Smith from...', 'Locating any information related to Jane Smith from...', 'Looking for specific information related to...', 'Between dates March and April 1941...', 'Any information about the division from...');
-    $natCertTitles = array('Beck, Daniel Frederick Christian, Cert 5353 issued 18 Feb 1867', 'Barsdorf, Solomon, Cert 2738 issued 11 Sept 1858', 'Anzarut, Jacob, Cert 5016 issued 26 Oct 1870', 'Fanty, Benno, Cert 5697 issued 24 Feb 1868', 'Edward, John, Cert 2700 issued 10 Jul 1858');
-?>
-
 <section class="row">
     <div class="col starts-at-full ends-at-two-thirds clr holding-box">
         <div class="heading-holding-banner">
@@ -19,74 +10,59 @@
                     <div class="date-filter-form">
                         <form>
                             <div class="field-row">
-                                <label for="date" style="display:none;">See orders in the past: </label>
+                                <!-- <label for="date">See orders in the past: </label> -->
                                 <select>
                                     <option value="0" label="3 months">3 months</option>
                                     <option value="1" label="6 months">6 months</option>
                                     <option value="2" label="12 months">12 months</option>
                                 </select>
                             </div>
-                        <!-- <input type="date" placeholder="DD/MM/YYYY">
-                        <input type="date" placeholder="DD/MM/YYYY">
-                        <input type="submit" value="Filter"> -->
                         </form>
                     </div>
                 </div>
             </div>
-            <ul id="your-orders">
-                <?php for ($i=0; $i < 5; $i++) {
-                    $rand = "RC " . rand(1000000, 9000000) . " 15";
-                    $randPrice = "£" . rand(10, 120) . ".00";
-                    $randDate = rand(01, 29) . "/" . rand(01, 12) . "/2015";
-                    $randStatus = $statuses[array_rand($statuses)];
-                    $randDigStatus = $digStatuses[array_rand($digStatuses)];
-                    $randFormat = $formats[array_rand($formats)];
-                    print("<li class='order-item'>");
-                    if ($randFormat == "Naturalisation certificate") {
-                        printf("<h3><a href='#'>%s</a></h3>", $natCertTitles[$i]);
-                    }
-                    else {
-                        printf("<h3><a href='#'>%s</a></h3>", $titles[$i]);
-                    }
-                    printf("<h3>Order Item ID: %s</h3>", $rand);
-                    printf("<span class='format-type'><strong>%s</strong></span>", $randFormat);
-                    printf("<p><strong>Date:</strong> %s</p>", $randDate);
-                    printf("<p><strong>Price:</strong> %s</p>", $randPrice);
-                    printf("<p><strong>Instructions:</strong> %s <a href='#'>read more</a></p>", $customerInst[$i]);
-                    if ($randFormat == "Digital download" && $randDigStatus == "Digital copy ready to download") {
-                        print("<div class='status action-this'>");
-                    }
-                    elseif ($randFormat == "Record copy" && $randStatus == "Page check successful") {
-                        print("<div class='status action-this'>");
-                    }
-                    elseif ($randFormat == "Naturalisation certificate" && $randStatus == "Page check successful") {
-                        print("<div class='status action-this'>");
-                    }
-                    else {
-                        print("<div class='status'>");
-                    }
-                    if ($randFormat == "Digital download") {
-                        printf("<p class='status-type'><strong>Status:</strong> %s</p>", $randDigStatus);
-                        if ($randDigStatus == "Digital copy ready to download") {
-                            print("<a class='button' href='#'>Download</a>");
-                        }
-                        else {
-                            print("<a href='#'>Enquire</a>");
-                        }
-                    }
-                    else {
-                        printf("<p class='status-type'><strong>Status:</strong> %s</p>", $randStatus);
-                        if ($randStatus == "Page check successful") {
-                            print("<a class='button' href='#'>Order copy</a>");
-                        }
-                        else {
-                            print("<a href='#'>Enquire</a>");
-                        }
-                    }
-                    print("</div></li>");
-                }
-                ?>
-            </ul>
+            <div class="your-order">
+                <div class="breather">
+                    <div class="your-order-header">
+                        <?php
+                        $rand = "1/15/" . rand(1000000, 9000000) . "M";
+                        $randDate = rand(01, 29) . "/" . rand(01, 12) . "/2015";
+                        printf("<h2>Order reference:</strong> %s</h2>", $rand);
+                        printf("<p><strong>Date:</strong> %s</p>", $randDate);
+                        ?>
+                    </div>
+                    <ul class="your-order-items">
+                        <?php require('index-page-order-loop.php'); ?>
+                    </ul>
+                    <div class="your-order-summary">
+                        <h3>Order 192837465 summary</h3>
+                        <p>Sub total: £358.00</p>
+                        <p>Postage and packing: £0.00</p>
+                        <p><strong>Total: £358.00</strong></p>
+                    </div>
+                </div>
+            </div>
+            <div class="your-order">
+                <div class="breather">
+                    <div class="your-order-header">
+                        <?php
+                        $rand = "1/15/" . rand(1000000, 9000000) . "M";
+                        $randDate = rand(01, 29) . "/" . rand(01, 12) . "/2015";
+                        printf("<h2>Order reference:</strong> %s</h2>", $rand);
+                        printf("<p><strong>Date:</strong> %s</p>", $randDate);
+                        ?>
+                    </div>
+                    <ul class="your-order-items">
+                        <?php require('index-page-order-loop.php'); ?>
+                    </ul>
+                    <div class="your-order-summary">
+                        <h3>Order 192837465 summary</h3>
+                        <p>Sub total: £358.00</p>
+                        <p>Postage and packing: £0.00</p>
+                        <p><strong>Total: £358.00</strong></p>
+                    </div>
+                </div>
+            </div>
             <ul class="menu-disc">
                 <li><a class="active">1</a></li>
                 <li><a>2</a></li>
@@ -103,8 +79,7 @@
             <ul class="sibling">
                 <li><a class="normal" href="/account/bookmarks">Your bookmarks</a></li>
                 <li><a class="normal" href="/account/tags">Your tags</a></li>
-                <li><a class="selected" href="/account/currentorders">Your current orders</a></li>
-                <li><a class="normal" href="/account/pastorders">Your previous orders</a></li>
+                <li><a class="selected" href="/account/your-orders">Your orders</a></li>
                 <li><a href="http://secure.nationalarchives.gov.uk/login/PersonalDetails">Your personal details</a></li>
             </ul>
         </div>
---
+++ includes/index-page-order-loop.php
@@ -0,0 +1,68 @@
+<?php
+$titles = array('42ND DIVISION Reconnaissance Battalion', '1ST ARMOURED DIVISION Commander Royal Engineers', '3RD INFANTRY BRIGADE Anti-tank Company', '1ST BATTALION Somerset Light Infantry', 'NAVY Medal card of Seddon, J Corps');
+$formats = array('Record copy', 'Digital download', 'Naturalisation certificate');
+$statuses = array('Page check in progress', 'Page check successful', 'Page check cancelled', 'Record copying order in progress', 'Record copying order complete and dispatched');
+$digStatuses = array('Digital copy ready to download', 'Digital copy download expired');
+$customerInst = array('Looking for information related to John Smith from...', 'Locating any information related to Jane Smith from...', 'Looking for specific information related to...', 'Between dates March and April 1941...', 'Any information about the division from...');
+$natCertTitles = array('Beck, Daniel Frederick Christian, Cert 5353 issued 18 Feb 1867', 'Barsdorf, Solomon, Cert 2738 issued 11 Sept 1858', 'Anzarut, Jacob, Cert 5016 issued 26 Oct 1870', 'Fanty, Benno, Cert 5697 issued 24 Feb 1868', 'Edward, John, Cert 2700 issued 10 Jul 1858');
+?>
+<?php
+    $randItems = rand(1, 5);
+    for ($i=0; $i < $randItems; $i++) {
+    $rand = "RC " . rand(1000000, 9000000) . " 15";
+    $randPrice = "£" . rand(10, 120) . ".00";
+    $randStatus = $statuses[array_rand($statuses)];
+    $randDigStatus = $digStatuses[array_rand($digStatuses)];
+    $randFormat = $formats[array_rand($formats)];
+    print("<li class='order-item'>");
+    if ($randFormat == "Naturalisation certificate") {
+        printf("<h3><a href='#'>%s</a></h3>", $natCertTitles[$i]);
+    }
+    else {
+        printf("<h3><a href='#'>%s</a></h3>", $titles[$i]);
+    }
+    printf("<h3>Order Item ID: %s</h3>", $rand);
+    printf("<span class='format-type'><strong>%s</strong></span>", $randFormat);
+    printf("<p><strong>Price:</strong> %s</p>", $randPrice);
+    if ($randFormat == "Digital download") {
+        print("<strong>Download details:</strong> Download size approximately 2MB<br><strong>Email address for digital item:</strong> test19@test.com</p>");
+    }
+    elseif ($randFormat == "Naturalisation certificate") {
+        print("<strong>Certified copy</strong></p>");
+    }
+    else {
+        printf("<p><strong>Instructions:</strong> %s <a href='#'>read more</a></p>", $customerInst[$i]);
+    }
+    if ($randFormat == "Digital download" && $randDigStatus == "Digital copy ready to download") {
+        print("<div class='status action-this'>");
+    }
+    elseif ($randFormat == "Record copy" && $randStatus == "Page check successful") {
+        print("<div class='status action-this'>");
+    }
+    elseif ($randFormat == "Naturalisation certificate" && $randStatus == "Page check successful") {
+        print("<div class='status action-this'>");
+    }
+    else {
+        print("<div class='status'>");
+    }
+    if ($randFormat == "Digital download") {
+        printf("<p class='status-type'><strong>Status:</strong> %s</p>", $randDigStatus);
+        if ($randDigStatus == "Digital copy ready to download") {
+            print("<a class='button' href='#'>Download</a>");
+        }
+        else {
+            print("<a href='#'>Enquire about this order item</a>");
+        }
+    }
+    else {
+        printf("<p class='status-type'><strong>Status:</strong> %s</p>", $randStatus);
+        if ($randStatus == "Page check successful") {
+            print("<a class='button' href='#'>Review order</a>");
+        }
+        else {
+            print("<a href='#'>Enquire about this order item</a>");
+        }
+    }
+    print("</div></li>");
+}
+?>
